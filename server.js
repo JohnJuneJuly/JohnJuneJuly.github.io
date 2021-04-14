541038068ac3f2df9952c8438f983d0128df21c7
John Her <johnjunejuly@hotmail.com>
2021-04-14 14:43:57 -0500

    added express and request and test page

--- server.js
+++ server.js
@@ -12,28 +12,41 @@ app.get("/", function(req, resp) { //send users to index file
     resp.sendFile(__dirname + "/index.html");
     // resp.sendFile(__dirname + "/index2.html");
 });
+app.get("/test", function(req, resp) { //send users to index file
+    // var request = require('request');
+    // var options = {
+    //     'method': 'GET',
+    //     'url': ''+url+'',
+    //     'headers': {
+    //         'Cookie': '__cfduid=d207d85138d8e495ac2e3bd4c5d6623fc1596672938'
+    //     }
+    // };
+    // request(options, function (error, response) {
+    //     if (error) throw new Error(error);
+    // //   console.log(response.body);
+    //     socket.emit('SEND_Data', response.body);
+    // });
+
+    resp.sendFile(__dirname + "/index2.html");
+    // resp.sendFile(__dirname + "/index2.html");
+});
+
+var request = require('request');
+var options = {
+    'method': 'GET',
+    // 'url': ''+url+'',
+    'url': 'https://api.opendota.com/api/players/162757300',
+    'headers': {
+        'Cookie': '__cfduid=d207d85138d8e495ac2e3bd4c5d6623fc1596672938'
+    }
+};
+request(options, function (error, response) {
+    if (error) throw new Error(error);
+    console.log(response.body);
+});
+
+// io.on('connection', (socket) => {
+//     console.log('a user connected');
+// });
+
 console.log("Server running at http://localhost:%d", port);
-
-
-io.on('connection', function(socket) {
-
-    socket.on('GET_Data', function(url){
-        var request = require('request');
-        var options = {
-            'method': 'GET',
-            'url': ''+url+'',
-            'headers': {
-                'Cookie': '__cfduid=d207d85138d8e495ac2e3bd4c5d6623fc1596672938'
-            }
-        };
-        request(options, function (error, response) {
-            if (error) throw new Error(error);
-        //   console.log(response.body);
-            socket.emit('SEND_Data', response.body);
-        });
-    }); //end of io function
-
-    socket.on('disconnect', function(){
-        // console.log("A user disconnected.")
-    });
-}); //end connection of IO connection
