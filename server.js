2f5ed360e64cd22aa9efbfdad5e9d7a6bdff52ae
John Her <johnjunejuly@hotmail.com>
2021-04-14 14:59:49 -0500

    added api

--- server.js
+++ server.js
@@ -27,23 +27,23 @@ app.get("/test", function(req, resp) { //send users to index file
     //     socket.emit('SEND_Data', response.body);
     // });
 
-    resp.sendFile(__dirname + "/index2.html");
+    resp.sendFile(__dirname + "/test.html");
     // resp.sendFile(__dirname + "/index2.html");
 });
 
-var request = require('request');
-var options = {
-    'method': 'GET',
-    // 'url': ''+url+'',
-    'url': 'https://api.opendota.com/api/players/162757300',
-    'headers': {
-        'Cookie': '__cfduid=d207d85138d8e495ac2e3bd4c5d6623fc1596672938'
-    }
-};
-request(options, function (error, response) {
-    if (error) throw new Error(error);
-    console.log(response.body);
-});
+// var request = require('request');
+// var options = {
+//     'method': 'GET',
+//     // 'url': ''+url+'',
+//     'url': 'https://api.opendota.com/api/players/162757300',
+//     'headers': {
+//         'Cookie': '__cfduid=d207d85138d8e495ac2e3bd4c5d6623fc1596672938'
+//     }
+// };
+// request(options, function (error, response) {
+//     if (error) throw new Error(error);
+//     console.log(response.body);
+// });
 
 // io.on('connection', (socket) => {
 //     console.log('a user connected');
